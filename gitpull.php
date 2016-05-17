5df8bac6805f77f6163d43534ad906f57da4564f
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-05-17 14:40:30 -0300

    Changed gitpull.php to use pm2

--- gitpull.php
+++ gitpull.php
@@ -1,43 +1,26 @@
 <?php
-    ini_set('display_errors', 'On');
-    error_reporting(E_ALL);
-    
-    if(file_exists('pid')){
-        $pid = trim(file_get_contents('pid'));
-        if($pid != ""){
-            exec("kill $pid", $out);
-            sendMessage("Git pull requested; killing bot with pid `$pid`");
-        } else {
-            sendMessage("pid is empty!");
-        }
-    } else {
-       sendMessage("pid file doesn't exist!");
-    }
-    
-    $result = shell_exec("/usr/bin/git pull 2>&1");
-    $url_result = urlencode($result);
-    sendMessage("Pull+result%3A%0D%0A`$url_result`", true);
-    echo $result;
-    
-    exec("/bin/bash start.sh >pid");
-    $pid = trim(file_get_contents("pid"));
-    if($pid != ""){
-        sendMessage("Bot restarted with pid `$pid`");
-    } else {
-        sendMessage("*WARNING* No pid returned from node process!");
-    }
-    
-    function sendMessage($message, $encoded = false){
-    	$env = file_get_contents('.env');
-    	$API_KEY = substr($env, 10);
-        $url = "https://api.telegram.org/bot$API_KEY/sendMessage?chat_id=-108631846&parse_mode=Markdown&text=";
-        if(!$encoded){
-            $message = urlencode($message);
-        }
-        $ch = curl_init($url . $message);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_exec($ch);
-        curl_close($ch);
-    }
-    
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+function sendMessage($message, $encoded = false){
+	$env = file_get_contents('.env');
+	$API_KEY = substr($env, 10);
+	$url = "https://api.telegram.org/bot$API_KEY/sendMessage?chat_id=-108631846&parse_mode=Markdown&text=";
+	if(!$encoded){
+		$message = urlencode($message);
+	}
+	$ch = curl_init($url . $message);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_exec($ch);
+	curl_close($ch);
+}
+
+$result = shell_exec("/usr/bin/git pull 2>&1");
+$url_result = urlencode($result);
+sendMessage("Pull+result%3A%0D%0A`$url_result`", true);
+echo $result;
+
+sendMessage("Git pull completed; restarting bot");
+exec("pm2 restart BeMEAN-bot");
+
 ?>
