07529a72199d8ae877947ce392e358f32fa28f1a
Suissa <jnascimento@gmail.com>
2016-05-18 10:43:05 -0300

    Teste msgDefault

--- modules/commands/start.js
+++ modules/commands/start.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const msgDefault = "Ops, nada por aqui ainda. Por favor, volte outro dia =)";
+// const msgDefault = "Ops, nada por aqui ainda. Por favor, volte outro dia =)";
+const msgDefault = "Sou muito tetudo e macio, porém por hora não tenho nada!";
+
 const execute = (msg, match, bot) => bot.sendMessage(msg.chat.id, msgDefault);
 module.exports = {
     'execute': execute,
