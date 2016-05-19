110ce76dad489375f31bca794c25ee8d23e6df62
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-05-19 14:39:13 -0300

    Added help command

--- modules/commands.js
+++ modules/commands.js
@@ -5,11 +5,13 @@ const ping = require('./commands/ping');
 const id = require('./commands/id');
 const start = require('./commands/start');
 const webschool = require('./commands/webschool');
+const help = require('./commands/help');
 
 module.exports = {
     'echo': echo,
     'ping': ping,
     'id': id,
     'start': start,
-    'webschool': webschool
+    'webschool': webschool,
+    'help': help
 }
---
+++ modules/commands/help.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// const msgDefault = "Ops, nada por aqui ainda. Por favor, volte outro dia =)";
+const msgDefault = "Ajuda com o que, meu filho?!";
+
+const execute = (msg, match, bot) => bot.sendMessage(msg.chat.id, msgDefault);
+module.exports = {
+    'execute': execute,
+    'numParams': 0
+}
