dcb86e7b6eca85ac6ce611b7630dd6d1e762b682
Suissa <jnascimento@gmail.com>
2016-05-19 16:18:53 -0300

    Modularizando

--- index.js
+++ index.js
@@ -10,28 +10,10 @@ const bot = new TelegramBot(token, { polling: true });
 const commands = require('./modules/commands');
 
 // Matches commands
-bot.onText(/\/([a-zA-Z]+) ?(.+)?/, (msg, match) => {
-    var command = match[1];
-    var args = match[2];
-    if (command) {
-        if (command in commands) {
-            command = commands[command];
-            if (match.length > command.numParams) {
-                if (args) {
-                    args = args.split(' ');
-                }
-                command.execute(msg, match, bot);
-            } else {
-                bot.sendMessage(msg.chat.id, "Ops, número incorreto de parâmetros fornecidos (" + match.length + "). Número de parâmetros exigidos: " + command.numParams + " :/");
-            }
-        } else {
-            bot.sendMessage(msg.chat.id, "Eita, esse comando não existe :/");
-        }
-    }
-});
+const all = require('./modules/commandsStatic/');
+bot.onText(/\/([a-zA-Z]+) ?(.+)?/, all);
 
 // Quem é
 // /^quem é/i
-bot.onText(/^quem é/i, (msg, match) => {
-  bot.sendMessage(msg.chat.id, "QUEM EH O QUE???");
-});
+const quemE = require('./modules/quemE/');
+bot.onText(/^quem é/i, quemE);
