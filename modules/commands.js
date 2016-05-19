79ae35813de626597aa87556fdd3baf5e331e746
Suissa <jnascimento@gmail.com>
2016-05-19 06:53:40 -0300

    testando

--- modules/commands.js
+++ modules/commands.js
@@ -4,10 +4,12 @@ const echo = require('./commands/echo');
 const ping = require('./commands/ping');
 const id = require('./commands/id');
 const start = require('./commands/start');
+const webschool = require('./commands/webschool');
 
 module.exports = {
     'echo': echo,
     'ping': ping,
     'id': id,
-    'start': start
+    'start': start,
+    'webschool': webschool
 }
