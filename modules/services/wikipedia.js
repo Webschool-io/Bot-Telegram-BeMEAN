609037235358180b8326df4f2c83fd5379c98bcb
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-05-20 12:43:42 -0300

    Added first

--- modules/services/wikipedia.js
+++ modules/services/wikipedia.js
@@ -16,13 +16,13 @@ var execute = (bot, msg, args) => {
                     if ($('#bodyContent #mw-content-text p:first').not('.coordinates').text() != "") {
                         var answer = $('#bodyContent #mw-content-text p:first').not('.coordinates').text();
                     } else {
-                        var answer = "Vish, não achei as coordenadas, mas aí vai a definição: " + $('#bodyContent #mw-content-text p').not('.coordinates').text().substr(0, 1000);
+                        var answer = "Vish, não achei as coordenadas, mas aí vai a definição: " + $('#bodyContent #mw-content-text p').not('.coordinates').first().text().substr(0, 1000);
                     }
                 } else {
                     var answer = $('#bodyContent #mw-content-text p.coordinates').text();
                 }
                 if (answer == "") {
-                    answer = $('#bodyContent #mw-content-text p').not('.coordinates').text().substr(0, 1000);
+                    answer = $('#bodyContent #mw-content-text p').not('.coordinates').first().text().substr(0, 1000);
                 }
                 bot.sendMessage(msg.chat.id, answer);
             } else if (error) {
