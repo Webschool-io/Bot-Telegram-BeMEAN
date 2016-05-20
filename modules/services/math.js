994089390bb54b4ec186b296ebe695a0c530d605
Suissa <jnascimento@gmail.com>
2016-05-20 17:52:31 -0300

    Corrigindo

--- modules/services/math.js
+++ modules/services/math.js
@@ -9,7 +9,8 @@
  */
  var execute = (bot, msg, args) => {
   const matchDate = /([0-9]{2}\/[0-9]{2}\/[0-9]{4}|[0-9]{2}\/20[0-9]{2})/;
-  if(!matchDate.test(msg.text)) bot.sendMessage(msg.chat.id, 'Calculando: ' + msg.text + ' = ' + eval(msg.text));
+  const _return = 'Calculando: `' + msg.text + ' = ' + eval(msg.text)+'`';
+  if(!matchDate.test(msg.text)) bot.sendMessage(msg.chat.id, _return);
 }
 
 module.exports = {
