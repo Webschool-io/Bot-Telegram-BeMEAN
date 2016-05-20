81aa5794dc4b489d087769faf40d0f71f3f3f607
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-05-20 10:55:13 -0300

    Changed stuff on wikipedia.js

--- modules/services/wikipedia.js
+++ modules/services/wikipedia.js
@@ -1,18 +1,17 @@
 'use strict';
 
-const api = require('wikipedia-js');
-
 var execute = (bot, msg, arg) => {
 
     const options = { query: arg, format: 'html', summaryOnly: true, lang: 'pt' };
     try {
         const cheerio = require('cheerio');
         const request = require('request');
+        console.log('iniciando');
         request('https://pt.wikipedia.org/wiki/Hakim_Bey', function (error, response, html) {
             if (!error && response.statusCode == 200) {
                 const $ = cheerio.load(html);
-                const teste = $('#bodyContent #mw-content-text p').text();
-                console.log('teste', teste)
+                const teste = $('#bodyContent #mw-content-text p:first').text();
+                console.log('teste', teste);
                 bot.sendMessage(msg.chat.id, teste);
             }
         });
