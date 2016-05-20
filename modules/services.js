01660166ac51a3aac869623d123abb7b3c248b2c
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-05-20 18:05:54 -0300

    mERGED

--- modules/services.js
+++ modules/services.js
@@ -2,19 +2,12 @@
 
 const wikipedia = require('./services/wikipedia');
 const math = require('./services/math');
-<<<<<<< HEAD
 const maconha = require('./services/maconha');
+const risada = require('./services/risada');
 
 module.exports = {
   wikipedia: wikipedia,
   math: math,
-  maconha: maconha
-=======
-const risada = require('./services/risada');
-
-module.exports = {
-  wikipedia: wikipedia
-, math: math
-, risada: risada
->>>>>>> fe6f5e6b955cffd0f26b2346ec0ad026d77836bd
+  maconha: maconha,
+  risada: risada
 }
