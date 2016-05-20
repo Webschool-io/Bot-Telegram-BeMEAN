931951ee2d5b5c5fbba7d6ec957a2fac5e3fa7a5
Suissa <jnascimento@gmail.com>
2016-05-20 18:43:19 -0300

    Faendo a busca

--- modules/services.js
+++ modules/services.js
@@ -4,10 +4,12 @@ const wikipedia = require('./services/wikipedia');
 const math = require('./services/math');
 const maconha = require('./services/maconha');
 const risada = require('./services/risada');
+const gmaps = require('./services/gmaps');
 
 module.exports = {
   wikipedia: wikipedia,
   math: math,
   maconha: maconha,
-  risada: risada
+  risada: risada,
+  gmaps: gmaps
 }
