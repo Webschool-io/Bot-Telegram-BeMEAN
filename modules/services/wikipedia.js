1ae2b125faf9bbefe3986597de3c4a7421000eac
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-05-20 11:21:02 -0300

    Added query string to wikipedia and improved error handling

--- modules/services/wikipedia.js
+++ modules/services/wikipedia.js
@@ -8,12 +8,20 @@ var execute = (bot, msg, arg) => {
         const cheerioAdv = require('cheerio-advanced-selectors');
         const cheerio = cheerioAdv.wrap(require('cheerio'));
         console.log('iniciando');
-        request('https://pt.wikipedia.org/wiki/Hakim_Bey', function (error, response, html) {
+        request('https://pt.wikipedia.org/wiki/' + args.replace(" ", "_"), function (error, response, html) {
             if (!error && response.statusCode == 200) {
                 const $ = cheerio.load(html);
                 const teste = $('#bodyContent #mw-content-text p:first').text();
                 console.log('teste', teste);
                 bot.sendMessage(msg.chat.id, teste);
+            } else if (error) {
+                const mili = new Date().getTime();
+                bot.sendMessage(msg.chat.id, "Droga, deu um erro aqui em :/ ID do erro: `" + mili + "`", { 'parse_mode': 'Markdown' });
+                console.log("Erro " + mili + ": " + error);
+            } else {
+                if (response.statusCode == 404) {
+                    bot.sendMessage(msg.chat.id, "Vish, a Wikipedia não tem nada sobre " + arg);
+                }
             }
         });
     } catch (e) {
