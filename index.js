83e59c49d87d5d1f9ee19847334f073e442837e1
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-05-20 12:12:53 -0300

    Console logged match

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ bot.onText(/^\/([a-zA-Z]+) ?([^@]+)?/, (msg, match) => {
 });
 
 bot.onText(/(Quem|O que|Onde) (é|eh|eah|e) ([^?]*)\??/i, (msg, match) => {
-    console.log("Recebi: " + msg.text);
+    console.log(match);
     services.wikipedia.execute(bot, msg, match[4]);
 });
 
