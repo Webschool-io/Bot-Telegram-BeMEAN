2e008e56c8ad5552e4afd4a551124682afe4f237
Suissa <jnascimento@gmail.com>
2016-05-20 15:54:37 -0300

    Com 2 espações

--- index.js
+++ index.js
@@ -12,45 +12,25 @@ const services = require('./modules/services');
 
 // Matches commands
 bot.onText(/^\/([a-zA-Z]+) ?([^@]+)?/, (msg, match) => {
-    var command = match[1];
-    var args = match[2];
-    if (command) {
-        if (command in commands) {
-            command = commands[command];
-            if (match.length > command.numParams) {
-                if (args) {
-                    args = args.split(' ');
-                }
-                command.execute(msg, match, bot);
-            } else {
-                bot.sendMessage(msg.chat.id, "Ops, número incorreto de parâmetros fornecidos (" + match.length + "). Número de parâmetros exigidos: " + command.numParams + " :/");
-            }
-        } else {
-            bot.sendMessage(msg.chat.id, "Eita, esse comando não existe :/");
+  const command = match[1];
+  const args = match[2];
+  if (command) {
+    if (command in commands) {
+      command = commands[command];
+      if (match.length > command.numParams) {
+        if (args) {
+            args = args.split(' ');
         }
+        command.execute(msg, match, bot);
+      } else {
+        bot.sendMessage(msg.chat.id, "Ops, número incorreto de parâmetros fornecidos (" + match.length + "). Número de parâmetros exigidos: " + command.numParams + " :/");
+      }
+    } else {
+      bot.sendMessage(msg.chat.id, "Eita, esse comando não existe :/");
     }
+  }
 });
 
 bot.onText(/(Quem|O que|O q|oq|Onde|Cadê|Cade) (é|eh|eah|e) ([^?]*)\??/i, (msg, match) => {
     services.wikipedia.execute(bot, msg, { 'wh': match[1], 'query': match[3] });
 });
-
-// 'use strict';
-
-// const TelegramBot = require('node-telegram-bot-api');
-// require('dotenv').config();
-
-// const token = process.env.API_TOKEN || 'INSERT API_TOKEN';
-// // Setup polling way
-// const bot = new TelegramBot(token, { polling: true });
-
-// const commands = require('./modules/commands');
-
-// // Matches commands
-// const all = require('./modules/commandsStatic/');
-// bot.onText(/\/([a-zA-Z]+) ?(.+)?/, all);
-
-// // Quem é
-// // /^quem é/i
-// const quemE = require('./modules/quemE/');
-// bot.onText(/^quem é/i, quemE);
