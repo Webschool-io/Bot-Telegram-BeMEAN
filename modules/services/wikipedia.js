e410835381b6d9e1cc3b1e300bd1ecb81eae878d
Suissa <jnascimento@gmail.com>
2016-05-20 19:13:51 -0300

    WIkipedia retornando no esquema!

--- modules/services/wikipedia.js
+++ modules/services/wikipedia.js
@@ -61,9 +61,9 @@ const escapeHTML = (code) =>
             }
 
             answer = (answer == "") ? answers.longDef : answer;
-            const _return = 'A Wikipédia diz que "' +simpleHTML(answer) + '". \nSaiba mais sobre <a href=\"#{_url}\">'+args.query.replace(" ", "_")+'</a>."';
+            const _return = 'A Wikipédia diz que "' +simpleHTML(answer) + '". Saiba mais sobre <a href=\""'+_url+'\"">'+args.query.replace(" ", "_")+'</a>."';
 
-            bot.sendMessage(msg.chat.id, _return.replace(/\[[^]]*\]/, ""), pm);
+            bot.sendMessage(msg.chat.id, _return, pm);
             break;
             case 404:
             bot.sendMessage(msg.chat.id, messages.noResultsFound + query);
