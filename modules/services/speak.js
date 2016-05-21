fdff59997a43876be7b9213c8ceab35b27ea46ee
Suissa <jnascimento@gmail.com>
2016-05-20 21:35:35 -0300

    Speak SIM

--- modules/services/speak.js
+++ modules/services/speak.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const fs = require('fs');
-const spawnSync = require('child_process').spawnSync;
+// const spawnSync = require('child_process').spawnSync;
+const execSync = require('exec-sync');
 
 const execute = (bot, msg, match) => {
   const _txt = msg.text.replace(/['"\\]/g, '');
   const fileName = Math.random().toString()+ '.mp3'
-  const out = spawnSync('sh', ['-c', "espeak -v pt --stdout '"+_txt+"' | avconv -i pipe:0 '"+fileName+"'"]);
+  const out = execSync("espeak -v pt --stdout '"+_txt+"' | avconv -i pipe:0 '"+fileName+"'");
   const audio = fs.readFileSync(fileName);
   bot.sendMessage(msg.chat.id, 'Enviando audio: '+fileName);
   bot.sendVoice(msg.chat.id, fileName);
