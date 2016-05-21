bcceb4578818a908615cfe60b1bbf3bd64c92392
Suissa <jnascimento@gmail.com>
2016-05-21 11:14:59 -0300

    Teste MD5

--- modules/services/md5.js
+++ modules/services/md5.js
@@ -4,7 +4,7 @@ const url = require('url');
 const md5 = require('md5');
 
 const execute = (bot, msg, match) => {
-  console.log('match.input.split('md5 ')[1]',match.input.split('md5 ')[1])
+  console.log('match.input 1',match.input.split('md5 ')[1])
   const query = match.input.split('md5 ')[1]
   bot.sendMessage(msg.chat.id, "MD5: " + md5(query));
 }
