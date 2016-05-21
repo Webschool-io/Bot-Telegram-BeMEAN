9be64c8a60e12b67c1866b45c98121a1bf2b3057
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-05-21 11:35:00 -0300

    Novas risadas adicionadas

--- modules/services/risada.js
+++ modules/services/risada.js
@@ -1,7 +1,16 @@
 'use strict';
 
+const risadas = [
+  'hehehehehe',
+  'hahahahaha',
+  'hauhauhauh',
+  'kkkkkkkkkk',
+  'asfdglkhjs',
+  'ri litros ahuah'
+]
+
 const execute = (bot, msg, args) => {
-  bot.sendMessage(msg.chat.id, 'hehehehehe');
+  bot.sendMessage(msg.chat.id, risadas[Math.floor(Math.random * risadas.length)]);
 }
 
 module.exports = {
