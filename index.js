013a8b6f96df4715115e54894d08147079dc0ce9
Suissa <jnascimento@gmail.com>
2016-05-20 22:16:30 -0300

    Pares

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ bot.onText(/\.filter/, (msg, match) => {
 });
 // Pares
 bot.onText(/\^par/, (msg, match) => {
-  const _arr = text.split('par ')[1]
+  const _arr = msg.text.split('par ')[1]
   const arr = JSON.parse(_arr);
   const _return =  arr.filter((acc)=> !(acc % 2));
   bot.sendMessage(msg.chat.id, 'Par(es): '+_return);
