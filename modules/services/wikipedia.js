59c34160a7ca9ef11abfe3d9b1ba7cbefbb52e4e
Suissa <jnascimento@gmail.com>
2016-05-23 16:25:20 -0300

    toLowerCase query

--- modules/services/wikipedia.js
+++ modules/services/wikipedia.js
@@ -87,7 +87,7 @@ const escapeHTML = (code) =>
  */
  var execute = (bot, msg, args) => {
     try {
-      const _url = 'https://pt.wikipedia.org/w/index.php?title=' + args.query.replace(" ", "_");
+      const _url = 'https://pt.wikipedia.org/w/index.php?title=' + args.query.toLowerCase().replace(" ", "_");
       request(_url, (err, res, html) => {
         parseResponse(err, res, html, args, bot, msg, _url);
       });
