26f392e72621578b3879916d9da21ca95c88d0bc
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-05-24 15:04:31 -0300

    refactored gme to be char insensitive

--- modules/services/gme.js
+++ modules/services/gme.js
@@ -3,9 +3,7 @@
 const url = require('url');
 
 const execute = (bot, msg, match) => {
-  const commmand = 'gme'
-  const arr = msg.text.split(commmand + ' ');
-  const cmd = arr.splice(1)[0];
+  const cmd = msg.text.replace(/gme /ig, '');
   const _url = 'http://pt-br.lmgtfy.com/?q=' + encodeURIComponent(cmd);
   bot.sendMessage(msg.chat.id, _url);
 }
