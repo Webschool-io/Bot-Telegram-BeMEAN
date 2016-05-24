4097e9c0380c36e07fe4523b819e43efec56f15a
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-05-24 12:12:21 -0300

    Alterado parse mode da wikipedia

--- modules/services/wikipedia.js
+++ modules/services/wikipedia.js
@@ -9,6 +9,7 @@ const cheerio = cheerioAdv.wrap(require('cheerio'));
 //Strings
 const regexOnde = /Onde|ond|cadê|cade/i;
 const pm = { 'parse_mode': 'Markdown' };
+const ph = { 'parse_mode': 'HTML' };
 const messages = {
     coordsNotFound: "*Vish, não achei as coordenadas, mas aí vai a definição: *\n",
     requestError: "Droga, deu um erro aqui em :/ ID do erro: `%mili%`",
@@ -19,57 +20,57 @@ const messages = {
 
 // Makes HTML more compatible to https://core.telegram.org/bots/api#html-style
 const simpleHTML = (code) =>
-  code.split(/\s+/m).join(' ')
-      .replace(/<\/?(p|h[1-6])[^>]*>/gi, '\n\n')
-      .replace(/<\/?(br|div|ol|ul)[^>]*>/gi, '\n')
-      .replace(/<li[^>]*>/gi, '  • ')
-      .replace(/<\/li[^>]*>/gi, '\n')
-      .replace(/(<(?!\/?(b|i|a|pre|code))[^>]+>)/g, '')
-      .replace(/(<[^\/]>[^<]*)<[^\/]>/g, '$1')
-      .replace(/<\/[^>]+>([^<]*<\/[^>]+>)/g, '$1')
-      .replace(/<[^>]*$/g, '')
-      .replace(/&#([0-9]+);/, (match,g1) => String.fromCharCode(g1))
-      .replace(/(\n\s*){3,}/g, '\n\n')
-      .replace(/^\s*|\s*$/g, '')
+    code.split(/\s+/m).join(' ')
+        .replace(/<\/?(p|h[1-6])[^>]*>/gi, '\n\n')
+        .replace(/<\/?(br|div|ol|ul)[^>]*>/gi, '\n')
+        .replace(/<li[^>]*>/gi, '  • ')
+        .replace(/<\/li[^>]*>/gi, '\n')
+        .replace(/(<(?!\/?(b|i|a|pre|code))[^>]+>)/g, '')
+        .replace(/(<[^\/]>[^<]*)<[^\/]>/g, '$1')
+        .replace(/<\/[^>]+>([^<]*<\/[^>]+>)/g, '$1')
+        .replace(/<[^>]*$/g, '')
+        .replace(/&#([0-9]+);/, (match, g1) => String.fromCharCode(g1))
+        .replace(/(\n\s*){3,}/g, '\n\n')
+        .replace(/^\s*|\s*$/g, '')
 
 const escapeHTML = (code) =>
-  code.replace(/&/gi, '&amp;')
-      .replace(/>/gi, '&gt;')
-      .replace(/</gi, '&lt;')
-      .replace(/"/gi, '&quot;')
+    code.replace(/&/gi, '&amp;')
+        .replace(/>/gi, '&gt;')
+        .replace(/</gi, '&lt;')
+        .replace(/"/gi, '&quot;')
 
 /**
  * Realiza o parse de uma response vinda do request
  */
- const parseResponse = (err, res, html, args, bot, msg, _url) => {
+const parseResponse = (err, res, html, args, bot, msg, _url) => {
 
     const query = args.query;
     const wh = args.wh;
     if (!err) {
         switch (res.statusCode) {
             case 200:
-            const $ = cheerio.load(html);
-            const answers = {
-                quickDef: $('#bodyContent #mw-content-text p:first').not('.coordinates').text(),
-                coordinates: $('#bodyContent #mw-content-text p.coordinates').text(),
-                longDef: $('#bodyContent #mw-content-text p').not('.coordinates').text().substr(0, 300)
-            };
+                const $ = cheerio.load(html);
+                const answers = {
+                    quickDef: $('#bodyContent #mw-content-text p:first').not('.coordinates').text(),
+                    coordinates: $('#bodyContent #mw-content-text p.coordinates').text(),
+                    longDef: $('#bodyContent #mw-content-text p').not('.coordinates').text().substr(0, 300)
+                };
 
-            var answer = answers.quickDef;
+                var answer = answers.quickDef;
 
-            if (wh.match(regexOnde)) {
-              answer = (answers.coordinates != "") ? answers.coordinates : messages.coordsNotFound + answers.longDef;
-            }
+                if (wh.match(regexOnde)) {
+                    answer = (answers.coordinates != "") ? answers.coordinates : messages.coordsNotFound + answers.longDef;
+                }
 
-            answer = (answer == "") ? answers.longDef : answer;
-            const _return = 'Segundo a Wikipédia: "_' + answer.replace(/\[[^]]*\]/, "") + '_". fonte: ' +_url;
+                answer = (answer == "") ? answers.longDef : answer;
+                const _return = 'Segundo a Wikipédia: "<i>' + answer.replace(/\[[^]]*\]/, "") + '</i>". fonte: ' + _url;
 
-            bot.sendMessage(msg.chat.id, _return, pm);
-            break;
+                bot.sendMessage(msg.chat.id, _return, ph);
+                break;
             case 404:
-            // bot.sendMessage(msg.chat.id, messages.noResultsFound + query);
-            duckduckgo.execute(bot, msg, args);
-            break;
+                // bot.sendMessage(msg.chat.id, messages.noResultsFound + query);
+                duckduckgo.execute(bot, msg, args);
+                break;
         }
     } else {
         const mili = new Date().getTime();
@@ -85,15 +86,15 @@ const escapeHTML = (code) =>
  * @param msg Objeto mensagem a ser utilizado para se obter  o id
  * @param args Objeto contento o tipo de pesquisa a realizar(wh) e o termo pesquisado (query)
  */
- var execute = (bot, msg, args) => {
+var execute = (bot, msg, args) => {
     try {
-      const _url = 'https://pt.wikipedia.org/w/index.php?title=' + args.query.toLowerCase().split(" ").join("_");
-      console.log(_url);
-      request(_url, (err, res, html) => {
-        parseResponse(err, res, html, args, bot, msg, _url);
-      });
+        const _url = 'https://pt.wikipedia.org/w/index.php?title=' + args.query.toLowerCase().split(" ").join("_");
+        console.log(_url);
+        request(_url, (err, res, html) => {
+            parseResponse(err, res, html, args, bot, msg, _url);
+        });
     } catch (e) {
-      bot.sendMessage(msg.chat.id, messages.communicationError.replace("%e%", e), pm);
+        bot.sendMessage(msg.chat.id, messages.communicationError.replace("%e%", e), pm);
     }
 }
 
