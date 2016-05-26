5d3669f98976a68a36ddfadd48ec83ececce5ae2
Suissa <jnascimento@gmail.com>
2016-05-26 19:30:05 -0300

    retirnado require do map

--- modules/services/eval-map.js
+++ modules/services/eval-map.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const _eval = 'map';
-const execute = (bot, msg, match) => bot.sendMessage(msg.chat.id, _eval + ': ' + eval(msg.text));
+const execute = (bot, msg, match) => {
+  if( !(/require/.test(msg.text)) )
+    bot.sendMessage(msg.chat.id, _eval + ': ' + eval(msg.text));
+};
 
 module.exports = {
   execute: execute
