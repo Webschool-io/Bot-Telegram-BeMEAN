65993021470c9dccf96a956f8112ad7e01e223cb
Suissa <jnascimento@gmail.com>
2016-05-26 10:33:36 -0300

    Colocando em service os stickers e ajeitando com atalho do ES6 para obetos

--- index.js
+++ index.js
@@ -125,20 +125,7 @@ bot.onText(/^([^\/]+)/i, (msg, match) => {
     {
       member: 'sticker-heart',
       regex: /(❤️|<3|S2)/i,
-      fn: (bot, msg) => {
-        const stickers = [
-          'BQADAgADVgADGgZFBFCh0QP4JfyUAg',
-          'BQADAQAD4AADeHUJBT9wFXPXtg5CAg',
-          'BQADAQADDgEAAs0wkgABkjFnUg42-BYC',
-          'BQADAgADqwEAAhlgSwTEm4bDHbGq_gI',
-          'BQADAQADpQEAArQUiAltLq7j1VlG0wI',
-          'BQADBAADCgQAAh8YpgAB-RH3g3ptUVEC',
-          'BQADBAADOQAD06wKAnzbafAKQh0LAg'
-        ];
-        const sticker = stickers[Math.floor(Math.random() * stickers.length)];
-        const reply = { 'reply_to_message_id': msg.message_id };
-        bot.sendSticker(msg.chat.id, sticker, reply);
-      }
+      fn: (bot, msg) => services.stickerHeart.execute(bot, msg, match)
     },
     {
       member: 'sticker-webschool',
