292adc1d27d7244cc488f54ee0edf7beb5a6c64b
Suissa <jnascimento@gmail.com>
2016-05-26 13:15:45 -0300

    stickr webschool

--- modules/services/sticker-webschool.js
+++ modules/services/sticker-webschool.js
@@ -1,8 +1,11 @@
 'use strict';
 
 const sticker = 'BQADAQADEwADt-CfBI-cLGYwa_u3Ag';
-const reply = { 'reply_to_message_id': msg.message_id };
-const execute = (bot, msg) => bot.sendSticker(msg.chat.id, sticker, reply);
+
+const execute = (bot, msg) => {
+  const reply = { 'reply_to_message_id': msg.message_id }
+  bot.sendSticker(msg.chat.id, sticker, reply);
+}
 
 module.exports = {
   execute: execute
