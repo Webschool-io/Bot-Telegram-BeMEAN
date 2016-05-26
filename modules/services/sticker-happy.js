912f1185dfbbbb248ab7959b5faf54d6fc696348
Suissa <jnascimento@gmail.com>
2016-05-26 11:57:34 -0300

    Memoization

--- modules/services/sticker-happy.js
+++ modules/services/sticker-happy.js
@@ -6,9 +6,20 @@ const stickers = [
   'BQADAgADrQEAAksODwAB4ac6Jt5y74UC',
   'BQADAgADgwEAAksODwABss0TWyMJO_YC'
 ];
+let memoization= []
 
+const memoize = (rand) => {
+  if(rand !== memoization[memoization.length-1])
+    memoization.push(rand);
+  else{
+    rand = _rand(stickers);
+    memoization.push(rand);
+  }
+}
+const _rand = (stickers) => Math.floor(Math.random() * stickers.length);
 const execute = (bot, msg) => {
-  const sticker = stickers[Math.floor(Math.random() * stickers.length)];
+  memoize(_rand(stickers));
+  const sticker = stickers[memoization.length-1];
   const reply = { 'reply_to_message_id': msg.message_id };
   bot.sendSticker(msg.chat.id, sticker, reply);
 }
