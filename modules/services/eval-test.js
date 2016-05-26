b5c9073c7611fc2bc8edd8ea40b6f09f631d1f46
Suissa <jnascimento@gmail.com>
2016-05-26 19:09:47 -0300

    ajeitando test()

--- modules/services/eval-test.js
+++ modules/services/eval-test.js
@@ -3,7 +3,9 @@
 const _eval = 'test';
 const execute = (bot, msg, match) => {
   console.log('msg', msg)
-  // bot.sendMessage(msg.chat.id, _eval + ': ' + eval(msg.text) );
+  const _test = msg.text.split('regex ')[1];
+  console.log('_test', _test)
+  bot.sendMessage(msg.chat.id, _eval + ': ' + eval(_test) );
 }
 
 module.exports = {
