ab5692bc1621bf29497095a2d68cfd63d46ca5a6
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-05-31 16:14:06 -0300

    Verificação de segurança adicionado ao service math

--- modules/services/math.js
+++ modules/services/math.js
@@ -3,10 +3,16 @@
 
 const pm = {'parse_mode': 'Markdown'};
 const execute = (bot, msg) => {
-	console.log('Eval recebido: ' + msg.text + ' de: ' + msg.from.id + ' ' + msg.from.first_name + ' ' + msg.from.last_name + ' ' + msg.from.username);
-  const matchDate = /([0-9]{2}\/[0-9]{2}\/[0-9]{4}|[0-9]{2}\/20[0-9]{2})/;
-  const _return = 'Calculando: `' + msg.text + ' = ' + eval(msg.text) + '`';
-  if (!matchDate.test(msg.text)) bot.sendMessage(msg.chat.id, _return, pm);
+  //const matchDate = /([0-9]{2}\/[0-9]{2}\/[0-9]{4}|[0-9]{2}\/20[0-9]{2})/;
+  if (isOk(msg.text)) {
+    const _return = 'Calculando: `' + msg.text + ' = ' + eval(msg.text) + '`';
+    //if (!matchDate.test(msg.text)) bot.sendMessage(msg.chat.id, _return, pm);
+    bot.sendMessage(msg.chat.id, _return, pm);
+  } else {
+    require('../monitutils').notifyAdmins(bot, 'Eval malicioso detectado: `' + msg.text + '`. Enviado por: ' + msg.from.id + ', ' + msg.from.first_name + ' ' + msg.from.last_name + ', @' + msg.from.username);
+    bot.sendMessage(msg.chat.id, 'Aaaaaaah! Espertinho você, em! Esse comando não é permitido não, jovem. O @osuissa e o @rmunhoz foram avisados sobre isso e, se pá, nunca mais respondo uma mensagem sua.');
+  }
+
 };
 
 module.exports = {
