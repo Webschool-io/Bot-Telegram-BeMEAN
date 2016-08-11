a42c787240cafdce433049bce08093865e03aeea
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-08-11 17:13:34 -0300

    Added treatment for ZERO_RESULTS status

--- modules/services/gmaps.js
+++ modules/services/gmaps.js
@@ -12,6 +12,10 @@ const api = new GoogleMapsAPI(config);
 const monitutils = require('../utils/monitutils');
 const errMsg = "Droga, ocorreu um erro ao processar a solicitação :/";
 
+const localeNotFound = (bot, query, result) => {
+  bot.sendMessage(msg.chat.id, "Então... Tem certeza que esse lugar existe? Pq procurei ele no Google Maps, e não achei, não :/");
+  monitutils.notifySharedAccount(bot, "Locale not found at gmaps service: " + query + "\nresult: " + encodeURIComponent(JSON.stringify(result)));
+}
 
 const execute = (bot, msg, match) => {
   // const query = msg.text.replace(/["'!?]/g, '');
@@ -30,12 +34,12 @@ const execute = (bot, msg, match) => {
     if (result.status != 'OK' || !result) {
       bot.sendMessage(msg.chat.id, errMsg);
       monitutils.notifySharedAccount(bot, "Erro no service do gmaps:\nQuery: `" + query + "`\nresult: `" + JSON.stringify(result) + "`");
+      if (result.status == 'ZERO_RESULTS') localeNotFound(bot, query, result);
       return;
     }
 
     if (!result.results[0]) {
-      bot.sendMessage(msg.chat.id, "Então... Tem certeza que esse lugar existe? Pq procurei ele no Google Maps, e não achei, não :/");
-      monitutils.notifySharedAccount(bot, "Locale not found at gmaps service: " + query + "\nresult: " + encodeURIComponent(JSON.stringify(result)));
+      localeNotFound(bot, query, result);
       return;
     }
 
