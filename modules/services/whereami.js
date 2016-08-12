05dc410e8d9dcb21607d552ae6496d76ca991d81
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-08-12 12:22:31 -0300

    Added reverse geocoding

--- modules/services/whereami.js
+++ modules/services/whereami.js
@@ -1,5 +1,20 @@
+const GoogleMapsAPI = require('googlemaps');
+const config = {
+  key: 'AIzaSyBnsCuuS0N0Akc1I3WEifbNoBCQ1iZ4a9g', //Não tente usar a chave, ela só aceita requests do meu server =)
+  secure: true
+}
+const api = new GoogleMapsAPI(config);
+const monitutils = require('../utils/monitutils');
+
 const execute = (bot, msg) => {
-    bot.sendMessage(msg.chat.id, 'Localização recebida: `' + JSON.stringify(msg) + '`');
+    let reverseParams = {
+        'latlng': msg.location.latitude + ',' + msg.location.longitude,
+        'language': 'pt-BR'
+    };
+    
+    api.reverseGeocode(reverseParams, (err, result) => {
+        bot.sendMessage(msg.chat.id, "Resultado: `" + JSON.stringify(result) + "`", {'parse_mode': 'markdown'});
+    });
 };
 
 module.exports = {
