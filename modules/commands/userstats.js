58b602b7b0ed6df6b4766c8fb7b605e9dfbb0110
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-08-25 17:26:03 -0300

    Fixed userstats message text

--- modules/commands/userstats.js
+++ modules/commands/userstats.js
@@ -6,7 +6,8 @@ const execute = (msg, match, bot) => {
     userutils.getUserCount((err, data) => {
         if (err) bot.sendMessage(msg.chat.id, "Erro ao obter a quantidade de usuários registrados: `" + JSON.stringify(err) + "`");
         else {
-            bot.sendMessage(msg.chat.id, "Atualmente, conheço " + data + data > 1 ? "pessoas" : "pessoa" + " =)");
+            let pessoa = data > 1 ? " pessoas" : " pessoa";
+            bot.sendMessage(msg.chat.id, ("Atualmente, conheço " + data + pessoa + " =)"));
         };
     });
 };
