5386078b02839472abc271e184f44a97c52b4e38
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-08-26 12:24:13 -0300

    Aded title and Markdown

--- modules/commands/example.js
+++ modules/commands/example.js
@@ -1,18 +1,19 @@
 'use strict';
 
 const execute = (msg, match, bot) => {
-    const text = 'Segundo o Qual é a Gíria: algo muito bom, excelente, ótimo.\nFonte: http://www.qualeagiria.com.br/giria/uva/'
+    const text = 'Segundo o Qual é a Gíria, *uva* é: algo muito bom, excelente, ótimo.\nFonte: http://www.qualeagiria.com.br/giria/uva/'
     bot.sendMessage(msg.chat.id, text, {
         reply_markup: {
             inline_keyboard: [[{
                 text: "Saiba como usar a gíria",
                 url: 'http://www.qualeagiria.com.br/giria/uva/'
             }]]
-        }
+        },
+        parse_mode: 'Markdown'
     });
 }
 
 module.exports = {
-  execute,
-  'numParams': 0
+    execute,
+    'numParams': 0
 };
