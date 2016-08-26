543c0ada20801e507d7499a4f27c265a20e0886a
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-08-26 12:25:05 -0300

    Markdown

--- modules/commands/example.js
+++ modules/commands/example.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const execute = (msg, match, bot) => {
-    const text = 'Segundo o Qual é a Gíria, *uva* é: algo muito bom, excelente, ótimo.\nFonte: http://www.qualeagiria.com.br/giria/uva/'
+    const text = 'Segundo o Qual é a Gíria\n*Uva*:\n`algo muito bom, excelente, ótimo.`\nFonte: http://www.qualeagiria.com.br/giria/uva/'
     bot.sendMessage(msg.chat.id, text, {
         reply_markup: {
             inline_keyboard: [[{
