8a258e1a5f5834927d0404c5519e11f799d37f8b
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-08-26 12:21:24 -0300

    fixed example

--- modules/commands/example.js
+++ modules/commands/example.js
@@ -2,7 +2,7 @@
 
 const execute = (msg, match, bot) => {
     const text = 'Segundo o Qual é a Gíria: algo muito bom, excelente, ótimo.\nFonte: http://www.qualeagiria.com.br/giria/uva/'
-    bot.sendMessage(msg.chat.id, , {
+    bot.sendMessage(msg.chat.id, text, {
         reply_markup: {
             inline_keyboard: [[{
                 text: "Saiba como usar a gíria",
