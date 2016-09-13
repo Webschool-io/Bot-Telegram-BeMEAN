08d4a412e0aff2bbd9a7eb458ab58b846758ef9f
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-09-13 09:50:49 -0300

    Added native Promisse API to mongoose

--- modules/db/index.js
+++ modules/db/index.js
@@ -3,6 +3,7 @@
 const mongoose = require('mongoose');
 const dbURI = 'mongodb://localhost/bemean';
 
+mongoose.Promise = global.Promise;
 mongoose.connect(dbURI);
 
 mongoose.connection.on('  ', function () {
