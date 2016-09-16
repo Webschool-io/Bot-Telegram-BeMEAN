5e4bcbbb28b542e851ba6d2525661ca8659858e0
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-09-16 16:21:12 -0300

    Fixed modules.js

--- modules/handlers/index.js
+++ modules/handlers/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  services: require('./serviceHandler'),
+  service: require('./serviceHandler'),
   sticker: require('./stickerHandler'),
   location: require('./locationHandler'),
   command: require('./commandHandler'),
