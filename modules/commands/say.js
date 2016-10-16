26572849d0e0812cb843b96452a566c041c7dd79
Rogério Munhoz <Rogrio Munhoz>
2016-10-16 18:23:43 -0200

    Implemented say command

---
+++ modules/commands/say.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const execute = (msg, match, bot) => {
+  if (match[2]) {
+    try {
+      const opts = match[2].match(/"(.*)" (.*)/);
+      const message = opts[1];
+      const ids = opts[2].split(" ");
+      if (message && ids.length > 0) {
+        ids.forEach((el) => {
+          bot.sendMessage(el, message, { parse_mode: 'Markdown' })
+            .then(() => {
+              bot.sendMessage(msg.chat.id, `Mensagem enviada para ${el}`);
+            })
+            .catch((e) => {
+              bot.sendMessage(msg.chat.id, `Erro ao enviar mensagem para o ID ${el}: \`${JSON.stringify(e)}\``, { parse_mode: 'Markdown' });
+            });
+        });
+      } else {
+        throw "Formato incorreto";
+      }
+    } catch (ignored) {
+      bot.sendMessage(msg.chat.id, "Uso: `/say \"mensagem\" IDs separados por expaço`", { parse_mode: 'Markdown' });
+    }
+  } else {
+    bot.sendMessage(msg.chat.id, 'Erro interno');
+  }
+}
