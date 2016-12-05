86e9ed66264986f4bf76ba8a06eb335de4f423ca
rmunhoz <rogerio.j.munhoz@gmail.com>
2016-12-05 13:21:22 -0200

    Started implementing cpf search

---
+++ modules/commands/cpf.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * @param msg
+ * @param msg.chat
+ * @param msg.chat.id
+ * @param match
+ * @param bot
+ */
+const execute = (msg, match, bot) => {
+  if(match[2] && match[2] != '') {
+    bot.sendMessage(msg.chat.id, `Procurando CPF ${match[2]} no cadastro do SUS`);
+  }
+}
+
+module.exports = {
+  'execute': execute
+  , 'numParams': 1
+};
