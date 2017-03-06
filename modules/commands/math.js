fd14ad6e9685ef6307fd21295849534139500be4
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2017-03-06 10:59:49 -0300

    Fixed math.js

--- modules/commands/math.js
+++ modules/commands/math.js
@@ -4,9 +4,9 @@ const math = require("mathjs");
 
 const execute = (msg, match, bot) => {
   if (match[1]) {
-    bot.sendMessage(msg.chat.id, `Resultado: \`${math.eval(match[2])}\``, {
-      parse_mode: "Markdown",
-      reply_to_message_id: msg.chat.id
+    const result = `\`${match[1]}\` = \`${math.eval(match[2])}\``;
+    bot.sendMessage(msg.chat.id, result, {
+      parse_mode: "Markdown"
     });
   } else {
     bot.sendMessage(msg.chat.id, "Cadê a expressão matemática, jovem? ¬¬''");
