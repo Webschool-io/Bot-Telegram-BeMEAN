2e761d5c41de5737e2c9f454f64d4ddb2126bc05
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2017-03-06 09:46:29 -0300

    Started using math.js for math service

--- modules/services/math.js
+++ modules/services/math.js
@@ -1,26 +1,10 @@
-'use strict';
+"use strict";
 
-const pm = { 'parse_mode': 'Markdown' };
-const isOk = require('../utils/regexutils').isInputOK;
-const safeEval = require('sewe');
-const s = require('../settings');
-
+const pm = {parse_mode: "Markdown"};
+const math = require("mathjs");
 const execute = (bot, msg) => {
-  if (s.getGlobal('evals', (err, data) => {
-    if (data === 'true') {
-      if (isOk(msg.text)) {
-        const _return = 'Calculando: `' + msg.text + ' = ' + safeEval(msg.text) + '`';
-        bot.sendMessage(msg.chat.id, _return, pm).catch(console.log);
-      } else {
-        require('../utils/monitutils').notifyAdmins(bot, 'Eval malicioso detectado: `' + msg.text + '`. Enviado por: ' + msg.from.id + ', ' + msg.from.first_name + ' ' + msg.from.last_name + ', @' + msg.from.username);
-        bot.sendMessage(msg.chat.id, 'Aaaaaaah! Espertinho você, em! Esse comando não é permitido não, jovem. O @osuissa e o @rmunhoz foram avisados sobre isso e, se pá, nunca mais respondo uma mensagem sua.').catch(console.log);
-      }
-    } else {
-      bot.sendMessage(msg.chat.id, 'Desculpa, evals estão desabilitados no momento');
-    }
-
-  }));
-
+  const _return = "Resultado: `" + msg.text + " = " + math.eval(msg.text) + "`";
+  bot.sendMessage(msg.chat.id, _return, pm).catch(console.log);
 };
 
 module.exports = {
