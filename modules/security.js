753dd130a3a163462b59f23b7a91c246b5cd469c
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2017-03-06 09:16:54 -0300

    Deactivated security check for evals

--- modules/security.js
+++ modules/security.js
@@ -1,10 +1,10 @@
 "use strict";
 
-const regexutils = require('./utils/regexutils');
-const userutils = require('./utils/userutils');
-const monitutils = require('./utils/monitutils');
+const regexutils = require("./utils/regexutils");
+const userutils = require("./utils/userutils");
+const monitutils = require("./utils/monitutils");
 
-const isSecure = (msg, isEval, cbk) => {
+/*const isSecure = (msg, isEval, cbk) => {
   let secure = true;
   if (!monitutils.isAdmin(msg.chat.id)) {
     userutils.isUserBlacklisted(msg.from.id, (err, data) => {
@@ -16,7 +16,10 @@ const isSecure = (msg, isEval, cbk) => {
       cbk(secure);
     });
   } else cbk(true)
-};
+};*/
+
+//Como nenhum eval está sendo executado, qualquer mensagem é segura
+const isSecure = (msg, isEval, cbk) => cbk(true);
 
 module.exports = {
   isSecure
