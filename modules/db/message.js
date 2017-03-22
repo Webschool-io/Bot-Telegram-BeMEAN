43b8dc0a968b9521c5bf2cad7caecef867ba4661
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2017-03-22 09:02:20 -0300

    Fixed syntax errors

--- modules/db/message.js
+++ modules/db/message.js
@@ -7,59 +7,57 @@ const pj = require('prettyjson');
 const Schema = mongoose.Schema;
 // Criação do Schema
 const jsonSchema = {
-  id: Number
-  , text: String
-  , timeReceived: Date
-  , msg: Schema.Types.ObjectId
+    id: Number
+    , text: String
+    , timeReceived: Date
+    , msg: Schema.Types.ObjectId
 };
 
 const messageSchema = new Schema(jsonSchema);
 
 messageSchema.virtual('blisted').get(function () {
-  return this.blacklisted.status;
+    return this.blacklisted.status;
 });
 
 const Message = mongoose.model('Message', messageSchema, 'messages');
 
 const callback = (err, data) => {
-  if (err) console.log('Erro no banco: ', err);
-  else console.log('Retorno do banco: ', data.result || data);
+    if (err) console.log('Erro no banco: ', err);
+    else console.log('Retorno do banco: ', data.result || data);
 };
 
 const Controller = {
-  insert: (d, cbk) => {
-    cbk = cbk || callback;
-    const model = new Message(d);
-    model.save(cbk);
-  },
-  select: (q, cbk) => {
-    cbk = cbk || callback;
-    Message.find(q, cbk);
-  },
-  delete: (q, cbk) => {
-    cbk = cbk || callback;
-    Message.remove(q, cbk);
-  },
-  log: msg => {
-    let model
-      , logText;
-    const timeReceived = new Date();
+    insert: (d, cbk) => {
+        cbk = cbk || callback;
+        const model = new Message(d);
+        model.save(cbk);
+    },
+    select: (q, cbk) => {
+        cbk = cbk || callback;
+        Message.find(q, cbk);
+    },
+    delete: (q, cbk) => {
+        cbk = cbk || callback;
+        Message.remove(q, cbk);
+    },
+    log: msg => {
+        let model;
+        const timeReceived = new Date();
 
-      model = new Message({
-        id: msg.id
-        , text: msg.text
-        , timeReceived
-        , msg: msg.text ? undefined : msg
-      });
+        model = new Message({
+            id: msg.id
+            , text: msg.text
+            , timeReceived
+            , msg: msg.text ? undefined : msg
+        });
 
-    model.save((err, result) => {
-      if (!err) console.log(`[LOG][${moment(timeReceived).format('DD/MM/YY HH:mm:ss')}] ${msg.text || 'Mensagem sem texto'}`);
-      else console.err(`[ERROR] Erro ao salvar mensagem ${msg.text || 'Mensagem sem texo'}: ${err}`);
-    });
-}, error: msg => {
-      console.log(`[ERROR][${moment(timeReceived).format('DD/MM/YY HH:mm:ss')}] ${pj.render(msg)}`);
-    });
-}
+        model.save()
+        .then(() => console.log(`[LOG][${moment(timeReceived).format('DD/MM/YY HH:mm:ss')}] ${msg.text || 'Mensagem sem texto'}`))
+        .catch(err => console.err(`[ERROR] Erro ao salvar mensagem ${msg.text || 'Mensagem sem texo'}: ${err}`));
+    }, error: msg => {
+        const timeReceived = new Date();
+        console.log(`[ERROR][${moment(timeReceived).format('DD/MM/YY HH:mm:ss')}] ${pj.render(msg)}`);
+    }
 };
 
 module.exports = Controller;
