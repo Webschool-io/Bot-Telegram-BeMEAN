b9e8c6dbbeb4e9b0dffb0c7dca040ea343c4171e
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2017-05-19 09:36:21 -0300

    Added more phrases and memory

--- modules/services/eval-date.js
+++ modules/services/eval-date.js
@@ -1,25 +1,39 @@
 'use strict';
 
-const _eval = 'Date';
-const isOk = require('../utils/regexutils').isInputOK;
-const safeEval = require('sewe');
+const answers = [
+  'Welcome to the jungle, baby!',
+  'Bem vindo ao Inferno!',
+  'Não quer entrar para tomar uma chícara de café?',
+  'Welcome, bitch :P',
+  'Aooooooooo fio! Bem vindo :D',
+  'Eaeeeeeeeeeee xD',
+  'Olaaaar ^^',
+  'Ih, alá, chegou mais um! Bem vindo :D'
+];
+
 const s = require('../settings');
 
-const execute = (bot, msg) => {
-  if (s.getGlobal('evals', (err, data) => {
-    if (data === 'true') {
-      if (isOk(msg.text)) {
-        bot.sendMessage(msg.chat.id, _eval + ': ' + safeEval(msg.text)).catch(console.log);
-      } else {
-        require('../utils/monitutils').notifyAdmins(bot, 'Eval malicioso detectado: `' + msg.text + '`. Enviado por: ' + msg.from.id + ', ' + msg.from.first_name + ' ' + msg.from.last_name + ', @' + msg.from.username);
-        bot.sendMessage(msg.chat.id, 'Aaaaaaah! Espertinho você, em! Esse comando não é permitido não, jovem. O @osuissa e o @rmunhoz foram avisados sobre isso e, se pá, nunca mais respondo uma mensagem sua.').catch(console.log);
-      }
-    } else {
-      bot.sendMessage(msg.chat.id, 'Desculpa, evals estão desabilitados no momento');
-    }
+const memory = {};
 
-  }));
+const execute = (bot, msg) => {
+  s.get(msg.chat.id, 'funny', (err, data) => {
+    if (data == 'true') _execute(bot, msg);
+  });
+}
+
+const _execute = (bot, msg) => {
+  bot.sendMessage(msg.chat.id, _getNewPhrase(msg)).catch(console.log);
 };
+
+const _getRandom = arr => arr[Math.floor(Math.random() * arr.length)];
+
+const _getNewPhrase = (msg) => {
+  let random
+  , mem = memory[msg.chat.id];
+  random = mem ? _getRandom(answers.filter(x => x != mem)) : _getRandomPhrase(answers);
+  memory[msg.chat.id] = random;
+  return random;
+}
 
 module.exports = {
   execute
--- modules/services/sair.js
+++ modules/services/sair.js
@@ -3,14 +3,18 @@
 const answers = [
   'Saiu porque não aguentou a pressão!',
   'Já vai tarde!',
-  'Tchau, querida!'
+  'Tchau, querida!',
   '“I’ll be back.”',
   'Que a força esteja com você!',
   'Hasta la vista, baby!',
-  'Pede pra sair, 01!'
+  'Pede pra sair, 01!',
+  'Falooooow!',
+  'Ih, alá, já vazou! :P'
 ];
 
 const s = require('../settings');
+
+const memory = {};
 
 const execute = (bot, msg) => {
   s.get(msg.chat.id, 'funny', (err, data) => {
@@ -21,6 +25,16 @@ const execute = (bot, msg) => {
 const _execute = (bot, msg) => {
   bot.sendMessage(msg.chat.id, answers[Math.floor(Math.random() * answers.length)]).catch(console.log);
 };
+
+const _getRandom = arr => arr[Math.floor(Math.random() * arr.length)];
+
+const _getNewPhrase = (msg) => {
+  let random
+  , mem = memory[msg.chat.id];
+  random = mem ? _getRandom(answers.filter(x => x != mem)) : _getRandomPhrase(answers);
+  memory[msg.chat.id] = random;
+  return random;
+}
 
 module.exports = {
   execute
