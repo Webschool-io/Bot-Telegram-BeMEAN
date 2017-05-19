85ff0adf082a53ed49a057ced175dcf9e0a2dc9a
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2017-05-19 09:39:51 -0300

    Added memory and some more phrases

--- modules/services/entrar.js
+++ modules/services/entrar.js
@@ -7,6 +7,7 @@ const answers = [
 ];
 
 const s = require('../settings');
+const memory = {};
 
 const execute = (bot, msg) => {
   s.get(msg.chat.id, 'funny', (err, data) => {
@@ -17,6 +18,16 @@ const execute = (bot, msg) => {
 const _execute = (bot, msg) => {
   bot.sendMessage(msg.chat.id, answers[Math.floor(Math.random() * answers.length)]).catch(console.log);
 };
+
+const _getRandom = arr => arr[Math.floor(Math.random() * arr.length)];
+
+const _getNewPhrase = (msg) => {
+  let random
+  , mem = memory[msg.chat.id];
+  random = mem ? _getRandom(answers.filter(x => x != mem)) : _getRandomPhrase(answers);
+  memory[msg.chat.id] = random;
+  return random;
+}
 
 module.exports = {
   execute
