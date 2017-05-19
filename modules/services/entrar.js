589fe7db3b269d1871929c039a49d5d90cba92c6
Rogério Munhoz <rjmunhoz@users.noreply.github.com>
2017-05-19 09:09:02 -0300

    Added memory
    Now the bot shouldn't send the same welcome message twice in a roll

--- modules/services/entrar.js
+++ modules/services/entrar.js
@@ -3,10 +3,17 @@
 const answers = [
   'Welcome to the jungle, baby!',
   'Bem vindo ao Inferno!',
-  'Não quer entrar para tomar uma chícara de café?'
+  'Não quer entrar para tomar uma chícara de café?',
+  'Welcome, bitch :P',
+  'Aooooooooo fio! Bem vindo :D',
+  'Eaeeeeeeeeeee xD',
+  'Olaaaar ^^',
+  'Ih, alá, chegou mais um! Bem vindo :D'
 ];
 
 const s = require('../settings');
+
+const memory = {};
 
 const execute = (bot, msg) => {
   s.get(msg.chat.id, 'funny', (err, data) => {
@@ -15,8 +22,24 @@ const execute = (bot, msg) => {
 }
 
 const _execute = (bot, msg) => {
-  bot.sendMessage(msg.chat.id, answers[Math.floor(Math.random() * answers.length)]).catch(console.log);
+  bot.sendMessage(msg.chat.id, _getNewPhrase(msg)).catch(console.log);
 };
+
+const _getRandomPhrase() => answers[Math.floor(Math.random() * answers.length)];
+
+const _getNewPhrase(msg) => {
+  let random
+  , mem = memory[msg.chat.id];
+  if(mem) {
+    do {
+      random = _getRandomPhrase();
+    } while (random == mem);
+    return random;
+  }
+  random = _getRandomPhrase();
+  memory[msg.chat.id] = random;
+  return random;
+}
 
 module.exports = {
   execute
