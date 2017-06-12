86380e6b50e479e4e7e30ef6050b7f2285072578
Gabriel Kalani <gabrielsilva1956@gmail.com>
2017-06-12 18:13:12 -0300

    try es6

--- modules/commands.js
+++ modules/commands.js
@@ -1,6 +1,44 @@
 "use strict";
 
-const echo = require("./commands/echo");
+import echo from "./commands/echo";
+import ping from "./commands/ping";
+import id from "./commands/id";
+import start from "./commands/start";
+import webschool from "./commands/webschool";
+import help from "./commands/help";
+import par from "./commands/par";
+import impar from "./commands/impar";
+import repo from "./commands/repo";
+import sendsticker from "./commands/sendsticker";
+import log from "./commands/log";
+import userstats from "./commands/userstats";
+import whitelist from "./commands/whitelist";
+import example from "./commands/example";
+import broadcast from "./commands/broadcast";
+import say from "./commands/say";
+import math from "./commands/math";
+
+export default {
+  echo,
+  ping,
+  id,
+  start,
+  webschool,
+  help,
+  par,
+  impar,
+  repo,
+  sendsticker,
+  log,
+  userstats,
+  whitelist,
+  broadcast,
+  say,
+  math
+};
+
+
+/*const echo = require("./commands/echo");
 const ping = require("./commands/ping");
 const id = require("./commands/id");
 const start = require("./commands/start");
@@ -35,4 +73,4 @@ module.exports = {
   broadcast,
   say,
   math
-};
+};*/
